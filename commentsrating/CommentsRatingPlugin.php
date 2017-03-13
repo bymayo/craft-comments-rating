3753b08bf8fc201cc3e56b7a44678d5f34f2561e
Jason Mayo <jason@bymayo.co.uk>
2017-03-13 21:26:00 +0000

    Corrected version number

--- commentsrating/CommentsRatingPlugin.php
+++ commentsrating/CommentsRatingPlugin.php
@@ -44,12 +44,12 @@ class CommentsRatingPlugin extends BasePlugin
 
     public function getVersion()
     {
-        return '1.0.1';
+        return '1.0.2';
     }
 
     public function getSchemaVersion()
     {
-        return '1.0.1';
+        return '1.0.2';
     }
 
     public function getDeveloper()
